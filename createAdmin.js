4949108f1d8e2271518fed5fe8acba07b3b20083
066097 <mjidhamza406@gmail.com>
2025-06-08 14:06:21 +0100

    Update admin email to orolux.store1@gmail.com in createAdmin.js log message

--- createAdmin.js
+++ createAdmin.js
@@ -4,10 +4,10 @@ require("dotenv").config();
 
 mongoose.connect(process.env.MONGO_URI || "YOUR_MONGO_URI_HERE").then(async () => {
   const admin = new AdminUser({
-    email: "orolux.stor1@gmail.com",
+    email: "orolux.store1@gmail.com",
     password: "Q!7v9z$Lp2@eR4wX" // Strong password
   });
   await admin.save();
-  console.log("Admin user created: orolux.stor1@gmail.com / Q!7v9z$Lp2@eR4wX");
+  console.log("Admin user created: orolux.store1@gmail.com / Q!7v9z$Lp2@eR4wX");
   mongoose.disconnect();
 });
